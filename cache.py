21868ec637ff587095eddb0f474452706dac83c6
julian <jdramirezl@eafit.edu.co>
2022-10-16 20:29:26 -0500

    Cache done!

--- cache.py
+++ cache.py
@@ -1,18 +1,24 @@
-class Node:
-    def __init__(self, req, res):
-        self.res = res
-        self.req = req
-        self.next = None
-        self.prev = None
+import sys
 
+# Definition of a Node for a Double Ended Queue
+class Node:
+    def __init__(self, req: str, res: str, TTL: int):
+        self.res : str = res
+        self.req : str = req
+        self.TTL : int = TTL
+        self.next : Node = None
+        self.prev : Node = None
+
+# Definition of a Double Ended Queue
 class DEQ:
     def __init__(self):
-        self.head = None
-        self.tail = None
-        self.size = 0
+        self.head : Node = None
+        self.tail : Node = None
+        self.size : int = 0
     
-    def add(self, data):
-        temp = Node(data)
+    # Add a new node to the front of the queue and return it
+    def add(self, data : list(str, str, int)):
+        temp = Node(*data)
         temp.next = self.head
         if self.head:
             self.head.prev = temp
@@ -25,38 +31,50 @@ class DEQ:
         
         return temp
     
-    def to_front(self, node):
+    # Move a node to the front of the queue
+    def to_front(self, node: Node):
         if not node or self.size == 0:
             return
         
         back, front = node.prev, node.next
+        
+        # If there is only one node in the queue or if the node is already the head, dont do anything
+        if (not back and not front) or (not back and front):
+            return 
+        
         node.prev, node.next = None, None
         
-        if not back and not front:
-            return 
-        elif not back and front:
-            front.prev = None
-        elif back and not front:
+        
+        if back and not front: # If the node is the tail, move to the front AND set the new tail
             back.next = None
-        else:
+            self.tail = back
+        else:                  # If the node is in the middle, move to the front
             front.prev = back
             back.next = front
         
+        # Move the node to the front
         node.next = self.head
+        
+        # And set the new head
         self.head = node
 
+    # Remove the last node from the queue and return it
     def pop(self):
+        # If the DEQ is empty, there is nothing to pop, so return None
         if self.size() == 0:
             return None
         
+        # Reduce size by 1
         self.size -= 1
         
+        # If there is only one node in the queue, set the head and tail to None and return the it
         if self.size() == 1:
             node = self.head
             node.next, node.prev = None, None
             self.head, self.tail = None, None
             return node
 
+        # If there are more than one node in the queue, set the tail to the second to last node and return the last node
         deleted = self.tail
         self.tail = deleted.prev
         deleted.prev, self.tail.next = None, None
@@ -64,39 +82,78 @@ class DEQ:
         return deleted
 
 class CACHE:
-    def __init__(self, path_deq: str, path_in_deq: str, max_size: int):
-        self.in_deq = {} # TODO: Read from file!~
-        self.deq = DEQ() # TODO: Read from file!~
-        self.DELIMITER = 'PENE' # TODO: Define delimiter
+    def __init__(self):
+        # Config variables
+        self.PATH_TO_PERSISTENCE : str = 'None' # TODO: read from config
+        self.MAX_SIZE : int = None # TODO: read from config
+        self.DELIMITER : str = None # TODO: read from config
+        self.TTL : int = None # TODO: read from config
+        self.UNIT_TIME : int = None # TODO: read from config
         
-        with open(path_in_deq, 'r') as file:
-            for line in file.readlines():
-                res, req = line.split(self.DELIMITER)
-                temp = Node(req, res)
-                self.deq.add(temp)
-                self.in_deq[req] = None
-                
+        # Cache variables
+        self.CURRENT_SIZE : int = 0
+        self.in_deq : dict = {} 
+        self.deq : DEQ = DEQ()
         
-        
-        dict = {
-            "get algo 110": Node
-        }
-        
-        self.MAX_SIZE = None # TODO: CHANGE TO ENV!
+        # Load cache from persistence
+        with open(self.persistence, 'r') as file:
+            for line in reversed(file.readlines()):
+                res, req, TTL : int = line.split(self.DELIMITER)
+                node = self.deq.add([req, res, TTL])
+                self.in_deq[req] = node
     
-    def add(self, request, response):
-        if self.deq.size == self.MAX_SIZE: 
-            self.deq.pop()
-            del self.in_deq[request]
+    def add(self, request: str, response: str):
+        # Check if cache is full and delete oldest nodes while it is
+        while self.CURRENT_SIZE + sys.getsizeof(response) > self.MAX_SIZE:
+            old_node = self.deq.pop()
+            del self.in_deq[old_node.req]
+            self.CURRENT_SIZE -= sys.getsizeof(old_node.res)
 
-        node = self.deq.add(request)
-        self.in_deq[request] = node # TODO: What is the req and the res?
-        return 1
+        # Add request to cache
+        node = self.deq.add([request, response, self.TTL])
+        self.CURRENT_SIZE += sys.getsizeof(response)
+        self.in_deq[request] = node
 
-    def get(self, request, response):
+    def get(self, request: str):
+        # Check if request is in cache
         if request in self.in_deq:
-            node = self.in_deq[request]
-            self.deq.to_front(node)
-            return node.data # TODO: RETORNAR EL RESPONSE! O EL ARCHIVO, LO QUE SEA!
+            node = self.in_deq[request] # Get node from cache
+            node.TTL = self.TTL # Reset TTL
+            self.deq.to_front(node) # Move node to front of cache
+            return node.res
         
-        return self.add(request, response)
+        # Request not in cache, return None
+        return None
+    
+    def update_time(self):
+        curr : Node = self.deq.tail
+        
+        # Iterate through cache
+        while curr != None:
+            node = curr
+            
+            # Check if TTL is 0 or less
+            node.TTL -= self.UNIT_TIME
+            
+            # If TTL is 0 or less, remove node from cache
+            if node.TTL <= 0:
+                self.deq.pop()
+                self.CURRENT_SIZE -= sys.getsizeof(node.res)
+                del self.in_deq[node.req]
+            
+            # Move to next node
+            curr = curr.prev
+    
+    def save_cache(self):
+        curr : Node = self.deq.head
+        
+        # Iterate through cache
+        while curr != None:
+            node = curr
+            
+            # Save node to persistence
+            with open(self.persistence, 'w') as file:
+                file.write(f'{node.res}{self.DELIMITER}{node.req}{self.DELIMITER}{node.TTL}'+'\n')
+            
+            # Move to next node
+            curr = curr.next
